fc193d53a67aaf744af3d8fb06342db355f76936
Willzy Dollarrzz <willzyofficial@gmail.com>
2024-11-30 12:47:19 +0100

    Created fluent.sh

--- fluent.sh
+++ fluent.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+
 echo "Installing dependencies..."
 sudo apt update && sudo apt upgrade -y
 
 if ! command -v nvm &> /dev/null; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
 fi
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
 
 nvm install 20
 npm install -g npm@latest
 nvm alias default 20
+
+if ! command -v cargo &> /dev/null; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+
+source "$HOME/.cargo/env"
 
 if ! command -v gblend &> /dev/null; then
   cargo install gblend
